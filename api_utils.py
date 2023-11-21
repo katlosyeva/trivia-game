f15e16fe52efd9b802729daffb4809fb31f7087e
Helen Vu <thinh_vu_25@msn.com>
2023-11-21 22:47:43 +0000

    changed some function names back

--- api_utils.py
+++ api_utils.py
@@ -1,7 +1,7 @@
 import requests
 
 
-def get_questions(amount, difficulty):
+def get_questions_from_api(amount, difficulty):
     url = f"https://opentdb.com/api.php?amount={amount}&difficulty={difficulty}&type=multiple"
     response = requests.get(url)
     return response.json()
